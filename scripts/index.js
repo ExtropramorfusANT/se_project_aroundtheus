2e35b06323fd8ffe5e65ab0fde305bb829cd7b60
Passione <J3l4n1evanz@gmail.com>
2025-03-16 13:52:07 -0400

    fixed some small issues that were noticed

--- scripts/index.js
+++ scripts/index.js
@@ -40,10 +40,13 @@ const addCardFormElement = addCardModal.querySelector(".modal__form");
 const addCardButton = document.querySelector("#add-card-button");
 const addCardCloseBtn = addCardModal.querySelector("#add-card-close-modal");
 const addCardForm = document.querySelector("#add-card-form");
+//Added query selector to card temp. why? -- I was told to. Cards won't enlarge. Where these 2 originate, check lines 144-115
 const cardTemplate = document
   .querySelector("#card-template")
   .content.querySelector(".card");
 
+const titleInput = document.querySelector("#card-title-input");
+const linkInput = document.querySelector("#card-link-input");
 const cardListEl = document.querySelector(".cards__list");
 /*Function*/
 
@@ -79,8 +82,8 @@ const cardUrlInput = addCardFormElement.querySelector(".modal__input_type_url");
 const cardTitle = document.querySelector(".card__title");
 const cardImage = document.querySelector(".card__image");
 
-function closeModal() {
-  profileEditModal.classList.remove("modal_opened");
+function closePopup(popup) {
+  popup.classList.remove("modal_opened");
 }
 
 const imageModal = document.querySelector("#image-modal");
@@ -92,7 +95,11 @@ function openImageModal(link, name) {
   modalImage.src = link;
   modalImage.alt = name;
   modalImageDescription.textContent = name;
-  imageModal.classList.add("modal_opened");
+  openPopup(imageModal);
+}
+
+function openPopup(popup) {
+  popup.classList.add("modal_opened");
 }
 
 //Event handler
@@ -107,12 +114,9 @@ function handleProfileEditSubmit(e) {
 addCardForm.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  const titleInput = document.querySelector("#card-title-input").value;
-  const linkInput = document.querySelector("#card-link-input").value;
-
   const cardData = {
-    name: titleInput,
-    link: linkInput,
+    name: titleInput.value,
+    link: linkInput.value,
   };
 
   const newCard = getCardElement(cardData);
